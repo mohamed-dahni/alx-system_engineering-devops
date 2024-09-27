3cd0b09292bd41fe8610177b9d3a77e597ae0dca
Mohamed <mdahni06@gmail.com>
2024-09-27 20:28:56 +0100

    +

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,2 +1,47 @@
-#!/usr/bin/env bash
-# This script INSERT DESCRIPTION HERE
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/**
+ * infinite_while - runs an infinite loop.
+ * 
+ * Return: 0 on success.
+ */
+int infinite_while(void)
+{
+    while (1)
+    {
+        sleep(1);
+    }
+    return (0);
+}
+
+int main(void)
+{
+    pid_t pid;
+    int i;
+
+    for (i = 0; i < 5; i++)
+    {
+        pid = fork();
+        if (pid > 0)
+        {
+            printf("Zombie process created, PID: %d\n", pid);
+        }
+        else if (pid == 0)
+        {
+            // Child process exits immediately to become a zombie
+            return (0);
+        }
+        else
+        {
+            // Fork failed
+            perror("Fork failed");
+            return (1);
+        }
+    }
+
+    infinite_while();
+    return (0);
+}
